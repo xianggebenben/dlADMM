d2b3301b1bc0b0ac22dfa8984409226e6da7524f
Junxiang Wang <xianggebenben@163.com>
2019-06-29 22:45:05 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,2 @@
+from distutils.core import setup
+setup(name="dlADMM", version="0.0.1", description="deep learning Alternating Direction Method of Multipliers", author="Junxiang Wang", py_modules=['dlADMM.common','dlADMM.input_data'])
