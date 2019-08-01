46f522cf66a1a81f42dce2ba7ba2350cc5d5fbd9
gmuadmin <gmuadmin@W104FZQLH2.mesa.gmu.edu>
2019-07-31 23:53:11 -0400

    Signed-off-by: gmuadmin <gmuadmin@W104FZQLH2.mesa.gmu.edu>

--- dlADMM/common.py
+++ dlADMM/common.py
@@ -58,30 +58,9 @@ def a_obj_gradient(a, W_next, b_next, z_next, u_next,v,z,rho):
     res=eq1_a(a, W_next, b_next, z_next, u_next,rho)+rho*(a-relu(z)+v)
     return res
 # return the result of W-subproblem
-def update_W(a_last, b, z, W_old, u,rho):
+def update_W(a_last, b, z, W_old, u,rho,alpha):
     gradients = eq1_W(a_last, W_old, b, z, u,rho)
-    gamma = 2
-    alpha = 1
-    zeta = W_old - gradients / alpha
-    while (eq1(a_last, zeta, b, z, u,rho) > P(zeta, alpha, a_last, W_old, b, z, u,rho)):
-        alpha = alpha * gamma
-        zeta = W_old - gradients / alpha  # Learning rate decreases to 0, leading to infinity loop here.
-    theta = alpha
-    W = zeta
-    return W
-# return the result of b-subproblem
-def update_b(a_last, W, z, b_old, u,rho):
-    gradients = eq1_b(a_last, W, b_old, z, u,rho)
-    res = b_old - gradients / rho
-    return res
-# return the objective value of z-subproblem
-def z_obj(a_last, W, b, z, u,v,a,rho):
-    f=(z-mul(W,a_last)-b+u/rho)*(z-mul(W,a_last)-b+u/rho)+(a-relu(z)+v)*(a-relu(z)+v)
-# return the result of W-subproblem
-def update_W(a_last, b, z, W_old, u,rho):
-    gradients = eq1_W(a_last, W_old, b, z, u,rho)
-    gamma = 2
-    alpha = 1
+    gamma = 10
     zeta = W_old - gradients / alpha
     while (eq1(a_last, zeta, b, z, u,rho) > P(zeta, alpha, a_last, W_old, b, z, u,rho)):
         alpha = alpha * gamma
@@ -132,10 +111,9 @@ def update_zl(a_last, W, b, label, zl_old, u,rho):
         zl = (1 - gamma) * zeta + gamma * zeta_old
     return zl
 # return the result of a-subproblem
-def update_a(W_next, b_next, z_next, z, a_old, u_next,v,rho):
+def update_a(W_next, b_next, z_next, z, a_old, u_next,v,rho,t):
     gradient = a_obj_gradient(a_old, W_next, b_next, z_next, u_next, v,z,rho)
     eta = 2
-    t=1
     beta=a_old-gradient/t
     while (a_obj(beta, W_next, b_next, z_next, u_next,v,z,rho) > Q(beta, t, a_old, W_next, b_next, z_next, u_next,v,z,rho)):
         t = t * eta
--- main.py
+++ main.py
@@ -75,8 +75,8 @@ y_test = np.swapaxes(y_test, 0, 1)
 x_test = np.array(x_test)
 y_test = np.array(y_test)
 
-num_of_neurons = 1000
-ITER = 1000
+num_of_neurons = 800
+ITER = 10000
 index = 0
 W1, b1, z1, a1, W2, b2, z2, a2, W3, b3, z3 = Net(x_train, y_train, num_of_neurons)
 u=np.zeros(z3.shape)
@@ -87,32 +87,36 @@ linear_r=np.zeros(ITER)
 objective_value=np.zeros(ITER)
 train_cost=np.zeros(ITER)
 test_cost=np.zeros(ITER)
-rho = 1e-6
+rho =1e-06
+tau1 =1
+tau2 =1
+theta1 =1
+theta2 =1
+theta3 =1
 # dlADMM
 for i in range(ITER):
     pre = time.time()
     print("iter=",i)
     z3 = common.update_zl(a2, W3, b3, y_train, z3, u, rho)
     b3 = common.update_b(a2, W3, z3, b3, u, rho)
-    W3 = common.update_W(a2, b3, z3, W3, u, rho)
-    a2 = common.update_a(W3, b3, z3, z2, a2, u, 0,rho)
+    W3 = common.update_W(a2, b3, z3, W3, u, rho,theta3)
+    a2 = common.update_a(W3, b3, z3, z2, a2, u, 0,rho,tau2)
     z2 = common.update_z(a1, W2, b2, a2, z2, 0, 0,rho)
     b2 = common.update_b(a1, W2, z2, b2, 0, rho)
-    W2 = common.update_W(a1, b2, z2, W2, 0, rho)
-    a1 = common.update_a(W2, b2, z2, z1, a1, 0, 0,rho)
+    W2 = common.update_W(a1, b2, z2, W2, 0, rho,theta2)
+    a1 = common.update_a(W2, b2, z2, z1, a1, 0, 0,rho,tau1)
     z1 = common.update_z(x_train, W1, b1, a1, z1, 0, 0,rho)
     b1 = common.update_b(x_train, W1, z1, b1, 0, rho)
-    W1 = common.update_W(x_train, b1, z1, W1, 0,rho)
-    W1 = common.update_W(x_train, b1, z1, W1, 0,rho)
+    W1 = common.update_W(x_train, b1, z1, W1, 0,rho,theta1)
+    W1 = common.update_W(x_train, b1, z1, W1, 0,rho,theta1)
     b1 = common.update_b(x_train, W1, z1, b1, 0,rho)
     z1 = common.update_z(x_train, W1, b1, a1,z1,0,0,rho)
-    a1 = common.update_a(W2, b2, z2, z1, a1, 0,0,rho)
-    W2 = common.update_W(a1, b2, z2, W2, 0,rho)
+    a1 = common.update_a(W2, b2, z2, z1, a1, 0,0,rho,tau1)
+    W2 = common.update_W(a1, b2, z2, W2, 0,rho,theta2)
     b2 = common.update_b(a1, W2, z2, b2, 0,rho)
-
     z2 = common.update_z(a1, W2, b2, a2,z2,0,0,rho)
-    a2 = common.update_a(W3, b3, z3, z2, a2, u,0,rho)
-    W3 = common.update_W(a2, b3, z3, W3, u,rho)
+    a2 = common.update_a(W3, b3, z3, z2, a2, u,0,rho,tau2)
+    W3 = common.update_W(a2, b3, z3, W3, u,rho,theta3)
     b3 = common.update_b(a2, W3, z3, b3, u,rho)
     z3 = common.update_zl(a2, W3, b3, y_train, z3, u, rho)
     u = u +rho*(z3-mul(W3,a2)-b3)
@@ -132,3 +136,11 @@ for i in range(ITER):
     print("test cost:", test_cost[i])
     print("test acc:",test_acc[i])
     print("Time per iteration:", time.time() - pre)
+    if i>0 and train_cost[i]>train_cost[i-1]:
+        rho=np.minimum(10*rho,0.1)
+        if num_of_neurons>=200:
+            tau1 = tau1 * 10
+            tau2 = tau2 * 10
+            theta1 = theta1 * 10
+            theta2 = theta2 * 10
+            theta3 = theta3 * 10
